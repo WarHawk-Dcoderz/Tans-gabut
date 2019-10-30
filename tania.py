14357102579b70ad77f560c92711c170c1791f69
tania27-cans <57148882+tania27-cans@users.noreply.github.com>
2019-10-30 22:07:20 +0700

    Update tania.py

--- tania.py
+++ tania.py
@@ -22,9 +22,10 @@ def cekip():
  print(f'[!] IP kamu : {ip}')
  
 def sms():
-os.system('cd sms')
+
  print(f'[!] Menginstall tools sms nya..')
- 
+ os.system('ls')
+os.system('cd sms')
  os.system('ls')
  os.system('python sms.py')
  
