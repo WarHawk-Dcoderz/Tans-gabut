e6166b7292c55c25f3d7375d3a2bd1510f802489
tania27-cans <57148882+tania27-cans@users.noreply.github.com>
2019-11-02 01:48:36 +0700

    Add files via upload

---
+++ websql.sh
@@ -0,0 +1,214 @@
+clear
+ echo    ❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜
+ echo   Author:Tania
+ echo    WA   : echo  echo echo https://wa.me/+628993307590
+ echo    ❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜
+
+ 
+ echo    echo echo http://www.crystalindonesia.co.id/news/newsdetail.php?id=3
+ echo  
+ echo    echo echo http://www.metanoiapublishing.com/publishing/author_detail.php?id=15
+ echo  
+ echo    echo echo http://jjstour.com/tours_detail.php?id=284
+ echo  
+ echo    echo echo http://www.citranashwatour.com/others.php?id=294
+ echo    echo echo http://www.jatengpromo.com/artikel.php?id=117
+ echo  
+  echo  
+  echo echo http://matadorcountry.com/detailnews.php?id=2485
+ echo  
+ echo   
+  echo echo http://www.rumahfiqih.com/artikel_.php?id=5
+ echo  
+ echo   
+  echo echo http://www.pudak-scientific.com/detail_products.php?id=259
+ echo  
+ echo  
+  echo echo http://www.rshva.com/berita-detil.php?id=6
+ echo  
+ echo  
+  echo echo http://www.vivocondom.com/news-detail.php?id=1
+ echo  
+ echo  
+  echo echo http://www.anekaholiday.com/product_detail.php?id=333
+ echo  
+ echo  
+  echo echo http://akademika.iba.ac.id/gtkarir/berita/fo-detail.php?id=8
+ echo  
+ echo  
+  echo echo http://sinemart.com/news.php?id=407
+ echo  
+ echo  
+  echo echo http://mypets.co.id/detailnews.php?id=15
+ echo  
+ echo  
+  echo echo http://www.aringocomputer.com/product.php?id=17
+ echo  
+ echo  
+  echo echo http://www.hasnurgroup.com/lowongan.php?id=1
+ echo  
+ echo  
+  echo echo http://www.savalihotel.com/holiday-index.php?id=1
+ echo  
+ echo  
+  echo echo http://krlmania.com/sinyal/read.php?id=2142
+ echo  
+ echo  
+  echo echo http://rsiabundasuryatni.com/artikel.php?id=142
+ echo  
+ echo  
+  echo echo http://home-klik.com/product_detail.php?id=112
+ echo  
+ echo  
+  echo echo http://seputarkediri.com/health/seputar-kediri.php?id=145
+ echo  
+ echo  
+  echo echo http://www.ptbnk.com/product/detail.php?id=1
+ echo  
+ echo  
+  echo echo http://www.pdw-architects.com/news.php?id=26
+ echo  
+ echo  
+  echo echo http://www.herjandi.com/article.php?id=21
+ echo  
+ echo  
+  echo echo http://www.produkabe.com/majakani.php?id=bisnisviden
+ echo  
+ echo  
+  echo echo http://pelitaindonesia.ac.id/hapusupload.php?id=116
+ echo  
+ echo  
+  echo echo http://hikmakaryapersada.com/artikel.php?id=32
+ echo  
+ echo  
+  echo echo http://www.krlmania.com/gado/read.php?id=703
+ echo  
+ echo  
+  echo echo http://www.dblindonesia.com/loop/newsdetail.php?id=10994
+ echo  
+ echo  
+  echo echo http://jagiradvertising.com/detail_produk.php?id=15
+ echo  
+ echo  
+  echo echo http://www.kafispolgama.or.id/umum_detail.php?id=2
+ echo  
+ echo  
+  echo echo http://www.indonesianship.com/beritaisi.php?ID=2582
+ echo  
+ echo  
+  echo echo http://www.vivocondom.com/news-detail.php?id=1
+ echo  
+ echo  
+  echo echo http://stasolar.com/kategori.php?id=6
+ echo  
+ echo  
+  echo echo http://roboticsindonesia.com/blog-view.php?id=36
+ echo  
+ echo  
+  echo echo http://www.kaoscouple-jakarta.com/readnews.php?id=16
+ echo  
+ echo  
+  echo echo http://www.tiketpesawatkita.com/?id=nanaz/artikel.php?id=18
+ echo  
+ echo  
+  echo echo http://www.imahseniman.com/room-detail.php?r=Junior
+ echo  
+ echo  
+  echo echo http://boslampu.com/help.php?id=7
+ echo  
+ echo  
+  echo echo http://citranashwatour.com/international.php?id=223
+ echo  
+ echo  
+  echo echo http://www.josstoday.com/single_foto.php?id=823
+ echo  
+ echo  
+  echo echo http://herjandi.com/event.php?id=12
+ echo  
+ echo  
+  echo echo http://www.home-klik.com/product_detail.php?id=32
+ echo  
+ echo  
+  echo echo http://www.sas-autoparts.com/toko.php?id=Semarang
+ echo  
+ echo  
+  echo echo http://www.pd-bprbkktemanggung.com/detail-produk.php?id=5
+ echo  
+ echo  
+  echo echo http://kssy-medan.com/konten.php?id=15
+ echo  
+ echo  
+  echo echo http://wahanatraining.com/page.php?id=3
+ echo  
+ echo  
+  echo echo http://www.andisucirta.com/blog_detail.php?id=70
+ echo  
+ echo  
+  echo echo http://www.thisiselma.com/id/news-detail.php?id=25
+ echo  
+ echo echo http://www.lila-travel.com/promo_murah.php?id=12
+ echo  
+ echo  
+ echo http://nalivimss.edu.bd/nali/view_gallery_meetings?page=1
+echo http://www.militaryhallofhonor.com/honoree-record.php?id=21
+echo http://www.hallmarksecurityleague.com/clubpage.php?id=22
+echo http://www.pphouse.org/journal_details.php?ijep=2&ID=55
+echo http://www.mclellandmusic.com/en_productdetail.php?id=34
+echo http://www.canterburyct.org/board.php?id=21
+echo http://www.sounditalia.it/dettagli_sliders.php?id=34
+echo http://sbss.edu.bd/sonar/view_gallery_meetings?page=1
+echo http://www.mclellandmusic.com/en_productdetail.php?id=34
+echo http://www.magitours.com/hotel_details.php?id=27
+echo https://troyanimalrescueproject.org/successdetail.php?ID=55
+echo http://www.wpc.com/news-media/full-article.php?id=44
+echo http://laylamalekia.edu.bd/layla/current_success_students_info?id=16
+echo http://www.hace.com.eg/pictures.php?id=3
+echo http://www.jhctechnology.cn/pro_show.php?id=34
+echo http://www.lawrencestomberg.com/blog_old.php?id=27
+echo http://www.benningtonsheriff.org/bcsd-media.php?id=55
+echo http://www.innofader.com/mixers.php?id=55
+echo http://www.greenlinearchitects.in/project_details.php?id=43
+echo http://www.amazonediamonds.com/products-detail.php?id=26
+echo http://www.flexilocation.com/details.php?id=44
+echo http://pro.southernhospitalitymagazine.com/people.php?id=3
+echo http://www.sw-optik.com/en/news_detail.php?id=53
+echo http://www.aayojan.edu.in/jaipur/news_detail.php?id=60
+echo http://nooralhayattourism.com/home_package_details.php?id=27
+echo http://www.clemcoindustries.com/news_detail.php?id=19
+echo http://www.t2production.fr/reference.php?id=44
+echo http://gargniwas.com/delux.php?cid=14
+echo http://southprecision.in/news_detail.php?id=22
+echo http://www.scottpetroleuminc.com/news_detail.php?id=21
+echo http://www.rena-metro.com/detail.php?id=97
+echo http://theclassichotel.in/service.php?id=15
+echo http://www.printellectual.com/printers.php?id=9
+echo http://www.zanesvillecars.com/vehicle.php?id=55
+echo http://mahabodhihotel.com/starhotel/room-detail.php?id=33
+echo http://www.waterandenergyprogress.org/case_studies.php?id=21
+echo http://judyswesternwares.com/proddetails.php?Id=55
+echo http://www.sasthagiriayyappatemple.com/photogallery_more.php?id=4
+echo http://wildeastadventure.com/destination.php?id=Ladakh
+echo http://www.shopclearviewmall.com/stores_detail.php?ID=44
+echo http://conceptcoms.com.kw/portfolio-inner.php?id=19
+echo http://www.clemcoindustries.com/news_detail.php?id=19
+echo http://dnake-global.com/system.php?id=34
+echo http://www.orion-timing.com/produits_detail.php?id=21
+echo http://www.rbaker.com/press-room.php?id=22
+echo http://doctormyfriend.com/display_articles.php?articleid=13
+echo http://www.andisucirta.com/blog_detail.php?id=44
+echo http://www.nanoentek.com/product.php?id=21
+echo http://www.humannageografia.sk/eng/people_detail.php?id=3
+echo http://www.martenjansson.com/manuals/db_download.php?id=34
+echo http://aranciakuchen.com/CERAMIC.php?cat_name=CERAMIC
+echo http://www.akwrfkerala.com/activities_more.php?id=8
+echo http://www.msspublicschool.org/other_aminities.php?fid=7
+echo http://www.victorialivengood.com/discography.php?id=21
+echo echo http://www.vinomaniawines.com/contentpage.php?id=34
+echo http://rostom.zakaria.free.fr/Swatta.php?id=34
+echo http://www.mmr-refrakter.com/eng/index.php?page=kategori&id=34
+echo http://thelexington.co.uk/event.php?id=21
+echo http://www.moosevalley.com/cms.php?id=34
+echo http://www.icomaa.com/articles.php?id=34
+echo http://www.sardanatutorials.com/videos.php?id=34
+echo http://arsaltis.com/en/detail_reference.php?id=34
+echo http://www.ethnicumbrella.com/product.php?cat=23
