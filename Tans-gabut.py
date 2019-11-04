698bcf8b21f4a039ae173aa418d5357aa51953ea
tania27-cans <57148882+tania27-cans@users.noreply.github.com>
2019-11-04 13:01:15 +0700

    Update Tans-gabut.py

--- Tans-gabut.py
+++ Tans-gabut.py
@@ -1,6 +1,6 @@
 
 import requests,os
-os.system('clear')
+
 
 print ("\033[1;32m❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜")
 print ("\033[1;31m[\033[1;37m1\033[1;31m] \033[1;33m Author=Tania`27")
@@ -8,8 +8,9 @@ print ("\033[1;31m[\033[1;37m1\033[1;31m] \033[1;33m WA   =08993307590")
 print ("\033[1;32m❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜")
 
  
+ 
 def tan():
-  print(f'[!] Menginstall tools Tania..')
+ print(f'[!] Menginstall tools Tania..')
  os.system('cd tania')
  os.system('python tania.py')
  
