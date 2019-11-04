50db69606392ec3ee15aa240db38f68f7f824cc7
tania27-cans <57148882+tania27-cans@users.noreply.github.com>
2019-11-04 13:08:25 +0700

    Add files via upload

---
+++ tanscans.py
@@ -0,0 +1,33 @@
+
+import requests,os
+os.system('clear')
+
+print ("\033[1;32m❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜")
+print ("\033[1;31m[\033[1;37m1\033[1;31m] \033[1;33m Author=Tania`27")
+print ("\033[1;31m[\033[1;37m1\033[1;31m] \033[1;33m WA   =08993307590")
+print ("\033[1;32m❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜")
+
+ 
+def tan():
+ print(f'[!] Menginstall tools Nya TANIA ..')
+
+ os.system('git clone https://github.com/tania27-cans/Tans-gabut')
+ os.system('cd Tans-gabut')
+ os.system('ls')
+ os.system('python Tans-gabut.py')
+
+print("\033[1;32m╬╬═══════════════╬╬")
+print ("\033[1;31m[\033[1;37m1\033[1;31m] \033[1;33m Install Tools Tania")
+print("\033[1;32m╬╬═══════════════╬╬")
+print ("\033[1;31m[\033[1;37m2\033[1;31m] \033[1;33m nggak jadi")
+print("\033[1;32m╬╬═══════════════╬╬")
+
+
+menu = input("\033[1;32m ketik 1: ")
+if menu =="1":tan()
+elif menu == '2':
+ print('[?] Keluar..')
+ sys.exit()
+else:
+ print('[?] Perintah tidak diketahui..')
+ sys.exit()
