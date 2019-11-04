03e025b5ae1f74ac0e0e4e7c6ba861ca3abe84f9
tania27-cans <57148882+tania27-cans@users.noreply.github.com>
2019-11-04 12:53:33 +0700

    Add files via upload

---
+++ Tans-gabut.py
@@ -0,0 +1,23 @@
+
+import requests,os
+os.system('clear')
+
+print ("\033[1;32m❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜")
+print ("\033[1;31m[\033[1;37m1\033[1;31m] \033[1;33m Author=Tania`27")
+print ("\033[1;31m[\033[1;37m1\033[1;31m] \033[1;33m WA   =08993307590")
+print ("\033[1;32m❛ ━━━━━･━━━━━━･━━━━━━･❪ ❁ ❫ ･━━━━━━ ━━━━━･━━━━━━･❜")
+
+ 
+def tan():
+ print(f'[!] Menginstall tools Nya TANIA ..')
+ def sms():
+ print(f'[!] Menginstall tools Tania..')
+ os.system('cd tania')
+ os.system('python tania.py')
+ 
+print("\033[1;32m╬╬═══════════════╬╬")
+print ("\033[1;31m[\033[1;37m1\033[1;31m] \033[1;33m Install Tools Tania")
+print("\033[1;32m╬╬═══════════════╬╬")
+
+menu = input("\033[1;32m ketik 1: ")
+if menu =="1":tan()
